5c167d95ea49aa84120ad7bb0e35334876da4d35
mj301296 <mrugankjadhav@gmail.com>
2023-06-23 17:47:57 -0700

    intro complete

---
+++ script.js
@@ -0,0 +1,35 @@
+// // const labelX = document.querySelector(`label[for="select-x"]`);
+// const input = document.querySelectorAll(`input[type="radio"]`)
+
+
+
+
+// function switchIcon(){
+//     console.log(this.name)
+//     console.log(this.checked)
+
+//     if(this.name===`X`)
+//     {
+//         const imgX= document.querySelector(`.imgX`)
+//         if(this.checked)
+//         {
+//             imgX.src = "./assets/icon-x-transparent.svg" 
+//         }
+//         else{
+//             imgX.src = "./assets/icon-x-grey.svg"
+//         }
+//     }
+// }
+// input.forEach(radio => radio.addEventListener('change',switchIcon))
+
+
+
+// // function changeIcon()
+// // {
+// //     console.log(this)
+// //     const input = document.querySelector(`input[id=${this.dataset.id}]`)
+// //     console.log(input.checked)
+// // }
+
+
+// // labelX.addEventListener('click',changeIcon)
